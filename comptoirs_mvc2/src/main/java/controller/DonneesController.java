199e3887b8ec93b7224c9c32eb37e8c62cb1e304
DonMarieLaure <marielaure.don@gmail.com>
2020-02-10 16:53:16 +0100

    donnee controller

--- comptoirs_mvc2/src/main/java/controller/DonneesController.java
+++ comptoirs_mvc2/src/main/java/controller/DonneesController.java
@@ -12,6 +12,7 @@ import javax.mvc.Controller;
 import javax.mvc.Models;
 import javax.mvc.View;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
@@ -23,58 +24,26 @@ import javax.ws.rs.QueryParam;
 @Path("mesdonnees")
 @View("mesdonnees.jsp")
 public class DonneesController {
+
     @Inject
     ClientFacade facade;
-    @Inject 
-    Models models;  
+    @Inject
+    Models models;
+
     @GET
-    public void AfficheClient(@QueryParam("code")String codeClient){
+    public void AfficheClient(@QueryParam("code") String codeClient) {
         Client c = facade.find(codeClient);
-        models.put("client",c);
+        models.put("client", c);
     }
-    public void changerContact(@QueryParam("contact") String referenceClient, String contact){
-        
+
+    @POST
+    public void changerContact(@QueryParam("code") String referenceClient, @QueryParam("contact") String contact) {
+
         Client c = facade.find(referenceClient);
         c.setContact(contact);
         facade.edit(c);
+        models.put("client", c);
+
     }
-    public void changerAdresse(@QueryParam("adresse") String referenceClient, String adresse ){
-        
-        Client c = facade.find(referenceClient);
-        c.setAdresse(adresse);
-        facade.edit(c);
-    }
-    public void changerFonction(@QueryParam("fonction") String referenceClient, String fonction){
-        
-        Client c = facade.find(referenceClient);
-        c.setFonction(fonction);
-        facade.edit(c);
-        
-    public void changerVille(@QueryParam("ville") String referenceClient, String fonction){
-        
-        Client c = facade.find(referenceClient);
-        c.setFonction(ville);
-        facade.edit(c);
-        
-    public void changerFonction(@QueryParam("region") String referenceClient, String fonction){
-        
-        Client c = facade.find(referenceClient);
-        c.setFonction(region);
-        facade.edit(c);
-        
-    public void changerFonction(@QueryParam("region") String referenceClient, String fonction){
-        
-        Client c = facade.find(referenceClient);
-        c.setFonction(region);
-        facade.edit(c);
-        
-    public void changerFonction(@QueryParam("code   postale") String referenceClient, String fonction){
-        
-        Client c = facade.find(referenceClient);
-        c.setFonction(code postale);
-        facade.edit(c);
-    }
-   // Faire Changer Ville,région, code postale, pays, téléphone, faxe
-    
-    
+
 }
